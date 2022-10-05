9601a070f87cb0c0f6598a7ebf97b199f9fe8151
Enow Mbi <benowmbi@yahoo.com>
2022-10-05 12:15:54 +0100

    Add password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_100402) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_111308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_100402) do
     t.jsonb "shopping_address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest", null: false
   end
 
 end
